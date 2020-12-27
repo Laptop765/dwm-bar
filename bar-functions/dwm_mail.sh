59c12e88bc81aff637c90cb0323fe1c671574219
Paul Berruti <paul@paulberruti.net>
2020-12-26 22:52:20 -0800

    Simplify top bar and add second email address

--- bar-functions/dwm_mail.sh
+++ bar-functions/dwm_mail.sh
@@ -7,17 +7,32 @@
 # To count mail in an inbox, change "/path/to/inbox" below to the location of your inbox. For example, "/home/$USER/.mail/new"
 
 dwm_mail () {
-    MAILBOX=$(ls /home/paul/.mail/laptop765-gmail/INBOX/new | wc -l)
+    MAILBOX1=$(ls /home/paul/.mail/laptop765-gmail/INBOX/new | wc -l)
+    MAILBOX2=$(ls /home/paul/.mail/offisignin-gmail/INBOX/new | wc -l)
 
     printf "%s" "$SEP1"
     if [ "$IDENTIFIER" = "unicode" ]; then
-        if [ "$MAILBOX" -eq 0 ]; then
-            printf "📪 %s" "$MAILBOX"
+        if [ "$MAILBOX1" -eq 0 ]; then
+            printf "📪 %s" "$MAILBOX1"
         else
-            printf "📫 %s" "$MAILBOX"
+            printf "📫 %s" "$MAILBOX1"
         fi
     else
-        printf "MAI %s" "$MAILBOX"
+        printf "MAI %s" "$MAILBOX1"
+    fi
+    # printf "%s\n" "$SEP2"
+
+    printf " | "
+
+    # printf "%s" "$SEP1"
+    if [ "$IDENTIFIER" = "unicode" ]; then
+        if [ "$MAILBOX2" -eq 0 ]; then
+            printf "📪 %s" "$MAILBOX2"
+        else
+            printf "📫 %s" "$MAILBOX2"
+        fi
+    else
+        printf "MAI %s" "$MAILBOX2"
     fi
     printf "%s\n" "$SEP2"
 }
